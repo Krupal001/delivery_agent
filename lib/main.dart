25667fdd0d02c9c2e3915ba1c301820afe00b29a
krupal001 <krupal24x7@gmail.com>
2024-02-28 18:48:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:delivery_agent/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
