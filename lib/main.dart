6d01c56d7e3e983e90fc7348dc1b85e8f6e274da
krupal001 <krupal24x7@gmail.com>
2024-02-29 18:55:22 +0530

    this official commit 2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:delivery_agent/screens/home_screen.dart';
+import 'package:delivery_agent/screens/notification/notification_services.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(const MyApp());
 }
+NotificationServices notificationServices=NotificationServices();
+@pragma('vm:entry-point')
+Future<void>_firebaseMessagingBackgroundHandler(RemoteMessage message)async{
+  await Firebase.initializeApp();
 
+
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -18,7 +27,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
+      themeMode:ThemeMode.system,
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,11 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
     // TODO: implement initState
     super.initState();
     notificationServices.requestNotificationPermission();
-    notificationServices.forgroundMessage();
     notificationServices.firebaseInit(context);
-    notificationServices.setupInteractMessage(context);
-    notificationServices.isTokenRefresh();
-
     notificationServices.getDeviceToken().then((value){
       if (kDebugMode) {
         print('device token');
@@ -42,49 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Flutter Notifications'),
       ),
-      body: Center(
-        child: TextButton(onPressed: (){
 
-          // send notification from one device to another
-          notificationServices.getDeviceToken().then((value)async{
-
-            var data = {
-              'to' : value.toString(),
-              'notification' : {
-                'title' : 'Asif' ,
-                'body' : 'Subscribe to my channel' ,
-                "sound": "jetsons_doorbell.mp3"
-              },
-              'android': {
-                'notification': {
-                  'notification_count': 23,
-                },
-              },
-              'data' : {
-                'type' : 'msj' ,
-                'id' : 'Asif Taj'
-              }
-            };
-
-            await http.post(Uri.parse('https://fcm.googleapis.com/fcm/send'),
-                body: jsonEncode(data) ,
-                headers: {
-                  'Content-Type': 'application/json; charset=UTF-8',
-                  'Authorization' : 'key=AAAAp9pXDFM:APA91bGhBeMCUABE2PXjl9UqodAZ2WdV_UI6PoiwdCzYaT8KeZmBKZszc01CD1GgN0OAJ1w3sNw9IVISyKhrrxQLASHizenGJUr2hjzoPjbjFu0HAx1CTk0l8Ut95ZENAQyRKm6hrltV'
-                }
-            ).then((value){
-              if (kDebugMode) {
-                print(value.body.toString());
-              }
-            }).onError((error, stackTrace){
-              if (kDebugMode) {
-                print(error);
-              }
-            });
-          });
-        },
-            child: Text('Send Notifications')),
-      ),
     );
   }
 }
